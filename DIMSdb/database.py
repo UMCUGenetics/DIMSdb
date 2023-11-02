2ef7cdd845f61a7914d8afaef28abe0dc43eaec1
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2023-11-02 18:09:19 +0100

    Add some patients

--- DIMSdb/database.py
+++ DIMSdb/database.py
@@ -1,11 +1,24 @@
-from sqlmodel import SQLModel, create_engine
-from .models import HMDB
+from sqlmodel import Session, SQLModel, create_engine
+from .models import Sample, Patient
 
-sqlite_file_name = "db.sqlite3"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+sqlite_file_name = "db.sqlite3"  # Todo: Move to config
+sqlite_url = f"sqlite:///{sqlite_file_name}"  # Todo: Move to config
 
 engine = create_engine(sqlite_url)
 
 
 def create_db_and_tables():
+    SQLModel.metadata.drop_all(engine)
     SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        p1 = Patient(intermediate_id="p1", birth_year=1989)
+        s1 = Sample(id="p1.1", type="blood", patient=p1)
+        s2 = Sample(id="p1.2", type="blood", patient=p1)
+
+        p2 = Patient(intermediate_id="p2", birth_year=1989)
+        s3 = Sample(id="p2.1", type="plasma", patient=p2)
+        s4 = Sample(id="p2.2", type="plasma", patient=p2)
+
+        session.add_all([p1, s1, s2, p2, s3, s4])
+        session.commit()
--- DIMSdb/main.py
+++ DIMSdb/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from sqlmodel import Session, select
 
 from .database import engine
-from .models import HMDB
+from .models import HMDB, Patient
 
 app = FastAPI()
 
@@ -17,4 +17,12 @@ def get_hmdb(id: str):
     with Session(engine) as session:
         query = select(HMDB).where(HMDB.id == id)
         result = session.exec(query).one_or_none()
+    return result
+
+
+@app.get("/patient/")
+def get_patients():
+    with Session(engine) as session:
+        query = select(Patient)
+        result = session.exec(query).all()
     return result
