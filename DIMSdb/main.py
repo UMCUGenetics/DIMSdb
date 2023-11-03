28ad48d6a5b197ae4dfd3b03989b879b1dbb317c
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2023-11-03 14:32:16 +0100

    Move functions

--- DIMSdb/main.py
+++ DIMSdb/main.py
@@ -9,23 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/hmdb/{id}")
-def get_hmdb(id: str):
-    with Session(engine) as session:
-        query = select(HMDB).where(HMDB.id == id)
-        result = session.exec(query).one_or_none()
-    return result
-
-
-@app.get("/patient/")
-def get_patients():
-    with Session(engine) as session:
-        query = select(Patient)
-        result = session.exec(query).all()
-    return result
+    return {"Hello": "DIMSdb user"}
 
 
 @app.get("/dimsrun/{name}")
@@ -33,6 +17,46 @@ def get_dimsrun(name: str):
     with Session(engine) as session:
         query = select(DIMSRun).where(DIMSRun.name == name)
         result = session.exec(query).one_or_none()
+    return result
+
+
+@app.get("/dimsrun/{name}/results")
+def get_dimsrun_result(name: str):
+    with Session(engine) as session:
+        query = select(DIMSResults).where(DIMSResults.run_name == name)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsrun/{name}/sample/{id}/results")
+def get_sample_result(name: str, id: str):
+    with Session(engine) as session:
+        query = select(DIMSResults).where(DIMSResults.run_name == name).where(DIMSResults.sample_id == id)
+        result = session.exec(query).one_or_none()
+    return result
+
+
+@app.get("/hmdbs/{id}")
+def get_hmdb(id: str):
+    with Session(engine) as session:
+        query = select(HMDB).where(HMDB.id == id)
+        result = session.exec(query).one_or_none()
+    return result
+
+
+@app.get("/patients/")
+def get_patients():
+    with Session(engine) as session:
+        query = select(Patient)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/patients/{id}")
+def get_patient():
+    with Session(engine) as session:
+        query = select(Patient).where(Patient.id == id)
+        result = session.exec(query).all()
     return result
 
 
@@ -49,20 +73,4 @@ def get_samples(id: str):
     with Session(engine) as session:
         query = select(Sample)
         result = session.exec(query).all()
-    return result
-
-
-@app.get("/dimsrun/{name}/results")
-def get_sample_result(name: str):
-    with Session(engine) as session:
-        query = select(DIMSResults).where(DIMSResults.run_name == name)
-        result = session.exec(query).all()
-    return result
-
-
-@app.get("/dimsrun/{name}/sample/{id}/results")
-def get_sample_result(name: str, id: str):
-    with Session(engine) as session:
-        query = select(DIMSResults).where(DIMSResults.run_name == name).where(DIMSResults.sample_id == id)
-        result = session.exec(query).one_or_none()
     return result
