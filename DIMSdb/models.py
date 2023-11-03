66acdac1d60bda0a347410b2e0311175bd82dba3
BasMonkey <20270816+BasMonkey@users.noreply.github.com>
2023-11-03 10:54:31 +0100

    Refactor of database with many-to-many relationship between dimsrun and hmdb table

--- DIMSdb/models.py
+++ DIMSdb/models.py
@@ -12,30 +12,39 @@ class DIMSRun(SQLModel, table=True):
     dims_results: List["DIMSResults"] = Relationship(back_populates='run')
 
 
+class DIMSResultsHMDBLink(SQLModel, table=True):
+    hmdb_uuid: Optional[int] = Field(
+        default=None, foreign_key="hmdb.uuid", primary_key=True
+    )
+    run_uuid: Optional[int] = Field(
+        default=None, foreign_key="dimsresults.uuid", primary_key=True
+    )
+
+
 class DIMSResults(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+    uuid: int = Field(default=None, primary_key=True)
     run_name: str = Field(max_length=50)
     polarity: bool = None # Positive = true, negative = false
     m_z: float = Field(primary_key=True)
     intensity: float = None
     z_score: float = None
 
-    hmdb_id: Optional[str] = Field(foreign_key='hmdb.id', max_length=11)
     sample_id: str = Field(foreign_key='sample.id')
     run_name: str = Field(foreign_key='dimsrun.name')
 
-    hmdb: Optional["HMDB"] = Relationship(back_populates='dims_results')
+    hmdb: List["HMDB"] = Relationship(back_populates='dims_results', link_model=DIMSResultsHMDBLink)
     sample: "Sample" = Relationship(back_populates='dims_results')
     run: "DIMSRun" = Relationship(back_populates='dims_results')
 
 
 class HMDB(SQLModel, table=True):
-    id: str = Field(primary_key=True, max_length=11)
+    uuid: int = Field(default=None, primary_key=True)
+    hmdb_id: str = Field(primary_key=True, max_length=14)
     name: str
     description: str
     MZ: float
 
-    dims_results: List["DIMSResults"] = Relationship(back_populates='hmdb')
+    dims_results: List["DIMSResults"] = Relationship(back_populates='hmdb', link_model=DIMSResultsHMDBLink)
 
 
 class Sample(SQLModel, table=True):
