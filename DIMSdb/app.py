2b0e9de861b7842ff6e2367254da34ffa98945e5
Anne Luesink <anneluesink@live.nl>
2024-03-25 15:32:23 +0100

    Changed file loop

--- DIMSdb/app.py
+++ DIMSdb/app.py
@@ -1,15 +1,18 @@
-from database import engine, SQLModel
 from read_rdata_testdata import parse_rdata
-# from .models import *
+import os
+from datetime import datetime
+
 
 def main():
     # SQLModel.metadata.drop_all(engine)
     # SQLModel.metadata.create_all(engine)
 
     path_name = "/Users/aluesin2/Documents/DIMSdb/test_data/"
-    run_names = ["RES_PL_20231002_plasma"]
+    run_names = [f for f in os.listdir(path_name) if not f.startswith('.')]
+    # run_names = ["RES_PL_20231002_plasma"]
     for run_name in run_names:
         print(run_name)
+        print(datetime.now())
         # file_neg = path_name + run_name + '/outlist_ident_space_negative.RData'
         file_neg = path_name + run_name + '/outlist_identified_negative.RData'
         print(file_neg)
@@ -18,6 +21,7 @@ def main():
         file_pos = path_name + run_name + '/outlist_identified_positive.RData'
         print(file_pos)
         parse_rdata(file_pos, run_name)
+
 
 if __name__ == "__main__":
     main()
