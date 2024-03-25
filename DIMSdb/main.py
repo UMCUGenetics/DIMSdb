e3dcc8026002b4cfdc2e99c0629e077623995aef
Anne Luesink <anneluesink@live.nl>
2024-03-25 15:38:35 +0100

    Added new db queries

--- DIMSdb/main.py
+++ DIMSdb/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from sqlmodel import Session, select
+from sqlmodel import Session, select, or_, join
 
 from .database import engine
-from .models import DIMSRun, DIMSResults, HMDB, Patient, Sample
+from .models import DIMSRun, DIMSResults, HMDB, Patient, Sample, DIMSResultsHMDBLink
 
 app = FastAPI()
 
@@ -12,7 +12,7 @@ def read_root():
     return {"Hello": "DIMSdb user"}
 
 
-@app.get("/dimsruns/")
+@app.get("/dimsruns")
 def get_dimsruns():
     with Session(engine) as session:
         query = select(DIMSRun)
@@ -39,8 +39,69 @@ def get_dimsrun_result(name: str):
 @app.get("/dimsruns/{name}/samples/{id}/results")
 def get_sample_result(name: str, id: str):
     with Session(engine) as session:
-        query = select(DIMSResults).where(DIMSResults.run_name == name).where(DIMSResults.sample_id == id)
-        result = session.exec(query).one_or_none()
+        query = select(DIMSResults).where(DIMSResults.run_name == name, DIMSResults.sample_id == id)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/results/samples/{id}")
+def get_dimsrun_samples(name: str, id: str):
+    with Session(engine) as session:
+        query = select(DIMSResults, Sample).join(Sample).where(DIMSResults.run_name == name,
+                                                               DIMSResults.sample_id == id)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/results/samples/{id}/hmdb")
+def get_dimsrun_samples(name: str, id: str):
+    with Session(engine) as session:
+        query = select(DIMSResults, HMDB).join(DIMSResultsHMDBLink,
+                                               onclause=DIMSResults.uuid == DIMSResultsHMDBLink.run_uuid) \
+            .join(HMDB, onclause=HMDB.uuid == DIMSResultsHMDBLink.hmdb_uuid).where(DIMSResults.run_name == name,
+                                                                                   DIMSResults.sample_id == id)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/samples/{id}/zscoremax/{zscoremax}")
+def get_sample_zscoremax_result(name: str, id: str, zscoremax: float):
+    with Session(engine) as session:
+        query = select(DIMSResults).where(DIMSResults.run_name == name,
+                                          DIMSResults.sample_id == id, DIMSResults.z_score >= zscoremax)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/samples/{id}/zscoremax/{zscoremax}/hmdb")
+def get_sample_zscoremax_result(name: str, id: str, zscoremax: float):
+    with Session(engine) as session:
+        query = select(DIMSResults, HMDB).join(DIMSResultsHMDBLink,
+                                               onclause=DIMSResults.uuid == DIMSResultsHMDBLink.run_uuid,
+                                               isouter=True) \
+            .join(HMDB, onclause=HMDB.uuid == DIMSResultsHMDBLink.hmdb_uuid, isouter=True).where(
+            DIMSResults.run_name == name,
+            DIMSResults.sample_id == id,
+            DIMSResults.z_score >= zscoremax)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/samples/{id}/zscoremin/{zscoremin}")
+def get_sample_zscoremin_result(name: str, id: str, zscoremin: float):
+    with Session(engine) as session:
+        query = select(DIMSResults).where(DIMSResults.run_name == name, DIMSResults.sample_id == id,
+                                          DIMSResults.z_score <= zscoremin)
+        result = session.exec(query).all()
+    return result
+
+
+@app.get("/dimsruns/{name}/samples/{id}/zscores/{zscoremin}/{zscoremax}")
+def get_sample_zscores_result(name: str, id: str, zscoremin: float, zscoremax: float):
+    with Session(engine) as session:
+        query = select(DIMSResults).where(DIMSResults.run_name == name, DIMSResults.sample_id == id,
+                                          or_(DIMSResults.z_score <= zscoremin, DIMSResults.z_score >= zscoremax))
+        result = session.exec(query).all()
     return result
 
 
@@ -52,7 +113,7 @@ def get_hmdb(id: str):
     return result
 
 
-@app.get("/hmdbs/")
+@app.get("/hmdbs")
 def get_hmdb():
     with Session(engine) as session:
         query = select(HMDB)
@@ -60,7 +121,7 @@ def get_hmdb():
     return result
 
 
-@app.get("/patients/")
+@app.get("/patients")
 def get_patients():
     with Session(engine) as session:
         query = select(Patient)
@@ -76,7 +137,7 @@ def get_patient(id: str):
     return result
 
 
-@app.get("/patients/{id}/samples/")
+@app.get("/patients/{id}/samples")
 def get_patient_samples(id: str):
     with Session(engine) as session:
         query = select(Sample).where(Sample.patient_id == id)
@@ -84,7 +145,7 @@ def get_patient_samples(id: str):
     return result
 
 
-@app.get("/samples/")
+@app.get("/samples")
 def get_samples():
     with Session(engine) as session:
         query = select(Sample)
